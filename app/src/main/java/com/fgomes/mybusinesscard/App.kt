4f0c063016255441b28e84e02dd8ea7bb6a54fd7
fernando gomes <fernando.gmsoli@gmail.com>
2021-09-02 20:25:29 -0300

    Updated UI MainActivity with recycler, view model and adapter

--- app/src/main/java/com/fgomes/mybusinesscard/App.kt
+++ app/src/main/java/com/fgomes/mybusinesscard/App.kt
@@ -1,7 +1,10 @@
 package com.fgomes.mybusinesscard
 
 import android.app.Application
+import com.fgomes.mybusinesscard.data.AppDatabase
+import com.fgomes.mybusinesscard.data.BusinessCardRepository
 
 class App: Application() {
-
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
