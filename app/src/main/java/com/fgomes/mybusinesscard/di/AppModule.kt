548bb1c35cf54c6bbc51c531a3faaf8df9279f60
fernando gomes <fernando.gmsoli@gmail.com>
2021-10-14 00:00:48 -0300

    refactor modules

--- app/src/main/java/com/fgomes/mybusinesscard/di/AppModule.kt
+++ app/src/main/java/com/fgomes/mybusinesscard/di/AppModule.kt
@@ -3,14 +3,13 @@ package com.fgomes.mybusinesscard.di
 import androidx.room.Room
 import com.fgomes.mybusinesscard.data.AppDatabase
 import com.fgomes.mybusinesscard.domain.repository.BusinessCardRepository
-import com.fgomes.mybusinesscard.domain.repository.DatabaseDataSource
 import com.fgomes.mybusinesscard.domain.usecase.*
 import com.fgomes.mybusinesscard.ui.addCard.AddBusinessCardViewModel
 import com.fgomes.mybusinesscard.ui.home.BusinessCardListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val db = module {
+val appModules = module {
     single {
         Room.databaseBuilder(
             get(),
@@ -23,21 +22,14 @@ val db = module {
     single {
         get<AppDatabase>().businessDao()
     }
-}
 
-val repositoryModule = module {
-    single<BusinessCardRepository> { DatabaseDataSource(businessCardDao = get()) }
-}
+    single { BusinessCardRepository(businessCardDao = get()) }
 
-val viewModelModule = module {
-    viewModel { BusinessCardListViewModel(getAllCardsUsecases = get()) }
-    viewModel { AddBusinessCardViewModel(createCardUseCase = get()) }
-}
+    factory { GetAllCardsUseCase(businessCardRepository = get()) }
+    factory { CreateCardUseCase(businessCardRepository = get()) }
+    factory { UpdateCardUseCase(businessCardRepository = get()) }
+    factory { DeleteCardUseCase(businessCardRepository = get()) }
 
-val domainModule = module {
-    factory<GetAllCardsUsecases> { GetAllCardsUseCaseImpl(businessCardRepository = get()) }
-    factory<CreateCardUseCase> { CreateCardUseCaseImpl(businessCardRepository = get()) }
-    factory<UpdateCardUseCase> { UpdateCardUseCaseImpl(businessCardRepository = get()) }
-    factory<DeleteCardUseCase> { DeleteCardUseCaseImpl(businessCardRepository = get()) }
-
+    viewModel { BusinessCardListViewModel(get()) }
+    viewModel { AddBusinessCardViewModel(get()) }
 }
