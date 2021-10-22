6304d3bd092f017bad8f177036df69edf4c1ee32
fernando gomes <fernando.gmsoli@gmail.com>
2021-10-22 15:05:34 -0300

    refactor AddBusinessCardFragment

--- app/src/main/java/com/fgomes/mybusinesscard/ui/addCard/AddBusinessCardFragment.kt
+++ app/src/main/java/com/fgomes/mybusinesscard/ui/addCard/AddBusinessCardFragment.kt
@@ -1,59 +1,137 @@
 package com.fgomes.mybusinesscard.ui.addCard
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.fgomes.mybusinesscard.R
+import com.fgomes.mybusinesscard.databinding.AddBusinessCardFragmentBinding
+import com.fgomes.mybusinesscard.domain.model.BusinessCard
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AddBusinessCardFragment : Fragment() {
 
+    private var _binding: AddBusinessCardFragmentBinding? = null
+    private val binding: AddBusinessCardFragmentBinding get() = _binding!!
     private val viewModel: AddBusinessCardViewModel by viewModel()
+    private val args: AddBusinessCardFragmentArgs by navArgs()
+
+    private lateinit var name: String
+    private lateinit var phone: String
+    private lateinit var email: String
+    private lateinit var business: String
+    private lateinit var fundo: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.add_business_card_fragment, container, false)
+        _binding = AddBusinessCardFragmentBinding.inflate(inflater, container, false)
+
+        createUpdateCard()
+        deleteCard()
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-    }
-
-    /*private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
-
-    /*private val mainViewModel: MainViewModel by viewModels {
-        MainViewModelFactory((application as App).repository) }*/
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        insertListeners()
-    }
-
-    private fun insertListeners() {
-        binding.btConfirm.setOnClickListener {
-            val businessCard = BusinessCard(
-                name = binding.tilName.editText?.text.toString(),
-                business = binding.tilBusiness.editText?.text.toString(),
-                phone = binding.tilPhone.editText?.text.toString(),
-                email = binding.tilEmail.editText?.text.toString(),
-                fundo = binding.tilCor.editText?.text.toString()
-            )
-            mainViewModel.insert(businessCard)
-            Toast.makeText(this, "Sucesso", Toast.LENGTH_SHORT).show()
-            finish()
-        }
-
-        binding.ibClose.setOnClickListener {
-            finish()
-        }
     }*/
 
+    private fun initFields() {
+        binding.let { businessCard ->
+            name = businessCard.tilName.editText?.text.toString()
+            phone = businessCard.tilPhone.editText?.text.toString()
+            business = businessCard.tilBusiness.editText?.text.toString()
+            email = businessCard.tilEmail.editText?.text.toString()
+            fundo = businessCard.tilCor.editText?.text.toString()
+        }
+    }
+
+    private fun createUpdateCard() {
+        if (args.businessCardArgs == null) {
+            newCard()
+        } else {
+            binding.tvTitle.setText(R.string.update_card)
+            updateCardGetArgs()
+        }
+    }
+
+    private fun updateCardGetArgs() {
+        args.businessCardArgs.let { businessCard ->
+            binding.run {
+                tilName.editText?.setText(businessCard?.name)
+                tilEmail.editText?.setText(businessCard?.email)
+                tilPhone.editText?.setText(businessCard?.phone)
+                tilBusiness.editText?.setText(businessCard?.business)
+                tilCor.editText?.setText(businessCard?.fundo)
+                btRemoveBusinessCard.visibility = View.VISIBLE
+                with(btAddUpdateBusinessCard) {
+                    text = getString(R.string.update_card)
+                    updateCard()
+                }
+            }
+        }
+    }
+
+    private fun newCard() {
+        binding.btAddUpdateBusinessCard.setOnClickListener {
+            initFields()
+            val businessCard = BusinessCard(0, name, phone, business, email, fundo)
+            if (name.isNotBlank() && name.isNotEmpty()) {
+                viewModel.insertCard(businessCard)
+            }
+            clearFields()
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun updateCard() {
+        binding.btAddUpdateBusinessCard.setOnClickListener {
+            initFields()
+            val businessCard =
+                BusinessCard(args.businessCardArgs?.id ?: 0, name, phone, business, email, fundo)
+            viewModel.updateCard(businessCard)
+            clearFields()
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun deleteCard() {
+        binding.btRemoveBusinessCard.setOnClickListener {
+            showRemoveDialog(args.businessCardArgs?.id ?: 0)
+        }
+    }
+
+    private fun showRemoveDialog(id: Int) {
+        AlertDialog.Builder(activity)
+            .setTitle(getString(R.string.dialog_title_delete))
+            .setMessage(getString(R.string.dialog_delete_msg))
+            .setNegativeButton(getString(R.string.label_no), null)
+            .setPositiveButton(getString(R.string.label_yes),
+                DialogInterface.OnClickListener { _, _ ->
+                    viewModel.deleteBusinesCard(id)
+                    clearFields()
+                    findNavController().popBackStack()
+                })
+            .create()
+            .show()
+    }
+
+    private fun clearFields() {
+        binding.tilName.editText?.text?.clear()
+        binding.tilPhone.editText?.text?.clear()
+        binding.tilBusiness.editText?.text?.clear()
+        binding.tilEmail.editText?.text?.clear()
+        binding.tilCor.editText?.text?.clear()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
