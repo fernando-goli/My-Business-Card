1f3417a73111b19d25405cab02184a0cd2bb7a7a
fernando gomes <fernando.gmsoli@gmail.com>
2021-10-22 15:07:42 -0300

    Refactor BussinesCardFragment

--- app/src/main/java/com/fgomes/mybusinesscard/ui/home/BusinessCardListFragment.kt
+++ app/src/main/java/com/fgomes/mybusinesscard/ui/home/BusinessCardListFragment.kt
@@ -8,14 +8,15 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.fgomes.mybusinesscard.R
 import com.fgomes.mybusinesscard.databinding.BusinessCardListFragmentBinding
-import com.fgomes.mybusinesscard.ui.adapter.BusinessCardAdapter
+import com.fgomes.mybusinesscard.domain.model.BusinessCard
+import com.fgomes.mybusinesscard.util.Image
+import com.fgomes.mybusinesscard.util.navigateWithAnimations
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class BusinessCardListFragment : Fragment() {
 
     private var _binding: BusinessCardListFragmentBinding? = null
     private val binding:  BusinessCardListFragmentBinding get() = _binding!!
-    private val adapter by lazy { BusinessCardAdapter() }
 
     private val viewModel: BusinessCardListViewModel by viewModel()
 
@@ -31,23 +32,49 @@ class BusinessCardListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        getAllBusinessCard()
-        insertListeners()
+        observerVM()
+        addNewCard()
     }
 
-    private fun insertListeners(){
+    private fun addNewCard(){
         binding.fab.setOnClickListener {
-            findNavController().navigate(R.id.action_businessCardListFragment_to_addBusinessCardFragment)
+            findNavController().navigateWithAnimations(
+                R.id.action_businessCardListFragment_to_addBusinessCardFragment)
         }
-
     }
 
-    private fun getAllBusinessCard() {
-        viewModel.getAll()
+    private fun observerVM() {
+       binding.run {
+
+           viewModel.getAll.observe(viewLifecycleOwner, { allCards ->
+
+               val businessCardAdapter = BusinessCardAdapter(allCards).apply {
+                   onItemClick = { businessCard ->
+                       navigateToAddCardFragment(businessCard)
+                   }
+
+                   listenerShare = { card ->
+                       Image.share(requireContext(), card)
+                   }
+               }
+
+               with(binding.rvMain) {
+                   setHasFixedSize(true)
+                   adapter = businessCardAdapter
+               }
+           })
+       }
     }
+
+    private fun navigateToAddCardFragment(businessCard: BusinessCard){
+        val direction = BusinessCardListFragmentDirections
+            .actionBusinessCardListFragmentToAddBusinessCardFragment(businessCard)
+        findNavController().navigateWithAnimations(direction)
+    }
+
 
     override fun onDestroy() {
-        super.onDestroy()
         _binding = null
+        super.onDestroy()
     }
 }
