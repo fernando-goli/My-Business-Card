4b31fcab3416ad5d386b9eed266dc88bde0a3f2f
fernando gomes <fernando.gmsoli@gmail.com>
2021-10-22 15:04:43 -0300

    Refactor AddBussinesCardViewModel

--- app/src/main/java/com/fgomes/mybusinesscard/ui/addCard/AddBusinessCardViewModel.kt
+++ app/src/main/java/com/fgomes/mybusinesscard/ui/addCard/AddBusinessCardViewModel.kt
@@ -1,7 +1,35 @@
 package com.fgomes.mybusinesscard.ui.addCard
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fgomes.mybusinesscard.domain.model.BusinessCard
+import com.fgomes.mybusinesscard.domain.usecase.CreateCardUseCase
+import com.fgomes.mybusinesscard.domain.usecase.DeleteCardUseCase
+import com.fgomes.mybusinesscard.domain.usecase.GetAllCardsUseCase
+import com.fgomes.mybusinesscard.domain.usecase.UpdateCardUseCase
+import kotlinx.coroutines.launch
 
-class AddBusinessCardViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AddBusinessCardViewModel(
+    private val getAllCardsUseCase: GetAllCardsUseCase,
+    private val createCardUseCase: CreateCardUseCase,
+    private val updateCardUseCase: UpdateCardUseCase,
+    private val deleteCardUseCase: DeleteCardUseCase
+) : ViewModel() {
+
+    private val getAll = getAllCardsUseCase()
+    val getAllCards: LiveData<List<BusinessCard>> get() = getAll
+
+    fun insertCard(businessCard: BusinessCard) = viewModelScope.launch {
+        createCardUseCase.createBusinessCard(businessCard)
+    }
+
+    fun updateCard(businessCard: BusinessCard) = viewModelScope.launch {
+        updateCardUseCase.update(businessCard)
+    }
+
+    fun deleteBusinesCard(id: Int) = viewModelScope.launch {
+        deleteCardUseCase.delete(id)
+    }
+
 }
